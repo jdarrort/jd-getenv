a03214ffb854c8b4b98f893b00c04c33cb52f49d
jdarrort <jdarrort@gmail.com>
2020-12-08 00:16:53 +0100

    fix undefined

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class EnvChecker {
             if ((value === undefined && arguments.length == 1) || !(/^-?\d+$/.test(value))) { throw 1};
             return parseInt(value) ;
         } catch (e) {
-            if (_default) return _default;
+            if (_default !== undefined) return _default;
         }
         throw new Error(`EnvChecker : ${envvar} is not an Int, and no default value`);
     }
@@ -20,22 +20,27 @@ class EnvChecker {
             if ((value === undefined && arguments.length == 1) || !(/^-?\d+$/.test(value))) { throw 1};
             return parseInt(value) ;
         } catch (e) {
-            if (_default) return _default;
+            if (_default !== undefined) return _default;
         }
         throw new Error(`EnvChecker : ${envvar} is not an Int, and no default value`);
     }    
     json(envvar, _default) {
         let val = this.get(envvar);
         try {
-            if (val === null || !(/^[\[|\{]/.test(val))) { throw 1 };
+            if (val === undefined || !(/^[\[|\{]/.test(val))) { throw 1 };
             return JSON.parse(val);
         } catch (e) {
-            if (_default) return _default;
+            if (_default !== undefined) return _default;
         }
         throw new Error(`EnvVarChecker : ${envvar} is not a JSON string, and no default value`);
     }
+    
     string(envvar, _default) {
-        return  this.get(envvar) || _default;
+        let val = this.get(envvar);
+        if (  val === undefined &&  arguments.length == 1 ) {
+            throw new Error(`EnvVarChecker : ${envvar} is not a STRING, and no default value`);
+        }
+        return  this.get(envvar) || _default;        
     }    
 }
 module.exports = new EnvChecker();
